30351b28c87035eada14101938129abff9a001ec
autobreaux <autobreaux@gmail.com>
2024-11-14 13:51:47 -0600

    updating CreateCSV.py

--- CreateCSV.py
+++ CreateCSV.py
@@ -11,92 +11,27 @@ mainpipeline = '/Volumes/Seagate/Magnetar_Automated_Pipeline/'
 with open('results_table.csv', 'w', newline='') as csv_file:
     writer = csv.writer(csv_file)
 
-    # Enter Source Folders #
-    for s in sourcelist[0]:
-        print(s)
-        new_src_folder = os.path.join(mainpipeline, s)
+    # Loop E-Bins #
+    for energy_bin in energybins[2]:
 
-        # Enter Energy Bin Folder 1 #
-        for t in energybins[2][0]:
-            print(t)
-            new_bin_folders = os.path.join(new_src_folder, t)
+        # Loop through Sources #
+        for source in sourcelist[0]:
 
-            # Target_results.txt Path #
-            txt_file_path = os.path.join(new_bin_folders, 'Target_results.txt')
-            if os.path.isfile(txt_file_path):
-                with open(txt_file_path, 'r') as txt_file:
-                    # Collect All Lines as a Single Row #
-                    rows = [line.strip().split() for line in txt_file]
-                    flat_row = [item for sublist in rows for item in sublist]
-                    
-                    # Write the Row Into CSV #
-                    writer.writerow(flat_row)
-            else:
-                continue
-    # Enter Source Folders #
-    for s in sourcelist[0]:
-        print(s)
-        new_src_folder = os.path.join(mainpipeline, s)
-
-        # Enter Energy Bin Folder 2 #
-        for t in energybins[2][1]:
-            print(t)
-            new_bin_folders = os.path.join(new_src_folder, t)
+            new_src_folder = os.path.join(mainpipeline, source)
+            new_bin_folder = os.path.join(new_src_folder, energy_bin)
 
             # Target_results.txt Path #
-            txt_file_path = os.path.join(new_bin_folders, 'Target_results.txt')
+            txt_file_path = os.path.join(new_bin_folder, 'Target_results.txt')
+
+            # Check for Target_results.txt #
             if os.path.isfile(txt_file_path):
+                
                 with open(txt_file_path, 'r') as txt_file:
-                    # Collect All Lines as a Single Row #
                     rows = [line.strip().split() for line in txt_file]
                     flat_row = [item for sublist in rows for item in sublist]
-                    
-                    # Write the Row Into CSV #
-                    writer.writerow(flat_row)
+
+                    # Write row into CSV #
+                    if flat_row:
+                        writer.writerow(flat_row)
             else:
-                continue
-    # Enter Source Folders #
-    for s in sourcelist[0]:
-        print(s)
-        new_src_folder = os.path.join(mainpipeline, s)
-
-        # Enter Energy Bin Folder 3 #
-        for t in energybins[2][2]:
-            print(t)
-            new_bin_folders = os.path.join(new_src_folder, t)
-
-            # Target_results.txt Path #
-            txt_file_path = os.path.join(new_bin_folders, 'Target_results.txt')
-            if os.path.isfile(txt_file_path):
-                with open(txt_file_path, 'r') as txt_file:
-                    # Collect All Lines as a Single Row #
-                    rows = [line.strip().split() for line in txt_file]
-                    flat_row = [item for sublist in rows for item in sublist]
-                    
-                    # Write the Row Into CSV #
-                    writer.writerow(flat_row)
-            else:
-                continue
-    # Enter Source Folders #
-    for s in sourcelist[0]:
-        print(s)
-        new_src_folder = os.path.join(mainpipeline, s)
-
-        # Enter Energy Bin Folder 4 #
-        for t in energybins[2][3]:
-            print(t)
-            new_bin_folders = os.path.join(new_src_folder, t)
-
-            # Target_results.txt Path #
-            txt_file_path = os.path.join(new_bin_folders, 'Target_results.txt')
-            if os.path.isfile(txt_file_path):
-                with open(txt_file_path, 'r') as txt_file:
-                    # Collect All Lines as a Single Row #
-                    rows = [line.strip().split() for line in txt_file]
-                    flat_row = [item for sublist in rows for item in sublist]
-                    
-                    # Write the Row Into CSV #
-                    writer.writerow(flat_row)
-            else:
-                continue
-    
+                print(f"File not found: {txt_file_path}")
